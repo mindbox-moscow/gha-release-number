2c3a6cecc0df71d483621a1076f7611a904b76e7
Roman Ivonin <roman.ivonin@gmail.com>
2019-11-08 17:08:42 +0300

    Implemented the release version generating logic

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  console.log(`Release the kraken.`);
+const defaultReleaseNumber = '1.0.0';
 
-  console.log(`The event payload: ${payload}`);
+const githubToken = core.getInput('token');
+const repoFullName = env.GITHUB_REPOSITORY;
 
-  core.setOutput("next-number", "1.2.3");
 
-} catch (error) {
-  core.setFailed(error.message);
+async function getNextReleaseNumber() {
+    const octokit = new github.GitHub(githubToken);
+
+    let releaseName = null;
+
+    [repoOwner, repoName] = repoFullName.split('/');
+
+    const latestRelease = await octokit.repos.getLatestRelease({
+        owner: repoOwner,
+        repo: repoName
+    })
+    .catch(error => {
+        if (error.status === 404) // no releases
+            releaseName = defaultReleaseNumber;
+        else
+            throw error;
+    });
+
+    if (releaseName != null)
+    {
+        console.log("No releases found, using the default relase number.");
+        return releaseName;
+    }
+
+    const tagRegex = /^v?(\d+)\.(\d+)\.(\d+)$/i;
+    const versionMatch = latestRelease.data.tag_name.match(tagRegex);
+
+    if (versionMatch == null)
+        throw new Error("Latest release doesn't have a tag in a form of 'X.X.X' / 'vX.X.X'");
+   
+    return`${versionMatch[1]}.${versionMatch[2]}.${parseInt(versionMatch[3]) + 1}`;
 }
+
+getNextReleaseNumber()
+    .then((releaseNumber) => {
+        console.log(`Release number: ${releaseNumber}`);
+        core.setOutput("release-number", releaseNumber);
+    })
+    .catch (err => {
+        core.setFailed(err.message);
+    });
